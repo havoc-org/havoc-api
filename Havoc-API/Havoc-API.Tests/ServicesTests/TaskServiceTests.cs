5a5e40b22f2ccff12a6093a56b16efb3dc7f9d83
Kyrylo Vlasenko <s24841@pjwstk.edu.pl>
2024-11-14 17:00:54 +0530

    Added tests for GetTasksByProjectIdAsync

--- Havoc-API/Havoc-API.Tests/ServicesTests/TaskServiceTests.cs
+++ Havoc-API/Havoc-API.Tests/ServicesTests/TaskServiceTests.cs
@@ -1,8 +1,78 @@
 using System;
+using FluentAssertions;
+using Havoc_API.Data;
+using Havoc_API.DTOs.Assignment;
+using Havoc_API.DTOs.Attachment;
+using Havoc_API.DTOs.Comment;
+using Havoc_API.DTOs.Tag;
+using Havoc_API.DTOs.Task;
+using Havoc_API.DTOs.TaskStatus;
+using Havoc_API.DTOs.User;
+using Havoc_API.Exceptions;
+using Havoc_API.Models;
+using Havoc_API.Services;
+using Havoc_API.Tests.TestData;
+using Xunit;
+using Task = Havoc_API.Models.Task;
+using TaskStatus = Havoc_API.Models.TaskStatus;
 
 namespace Havoc_API.Tests.ServicesTests;
 
 public class TaskServiceTests
 {
+    private readonly IHavocContext _context;
+    private readonly ITaskService _taskService;
+    
+    public TaskServiceTests()
+    {
+        _context = HavocTestContextFactory.GetTestContext();
 
+        _taskService = new TaskService(_context);
+    }
+
+    [Fact]
+    public async System.Threading.Tasks.Task TaskService_GetTasksByProjectIdAsync_ReturnsAllTasksInProject_WhenProjectExisted()
+    {
+        //Arange
+        var user = HavocTestContextFactory.CreateTestUser();
+        await _context.Users.AddAsync(user);
+        await _context.SaveChangesAsync();
+
+        var project = HavocTestContextFactory.CreateTestProject(user);
+        await _context.Projects.AddAsync(project);
+        await _context.SaveChangesAsync();
+
+        var task1 = HavocTestContextFactory.CreateTestTask(user, project);
+        var task2 = HavocTestContextFactory.CreateTestTask(user, project);
+        var taskList = new List<Task>([task1, task2]);
+        await _context.Tasks.AddRangeAsync(taskList);
+        await _context.SaveChangesAsync();
+
+        var taskGet1 = HavocTestContextFactory.CreateTestTaskGET(task1);
+        var taskGet2 = HavocTestContextFactory.CreateTestTaskGET(task2);
+
+        //Act
+        var tasks = await _taskService.GetTasksByProjectIdAsync(project.ProjectId);
+
+        //Assert
+        tasks
+            .Should()
+            .BeAssignableTo<IEnumerable<TaskGET>>()
+            .And.HaveSameCount(taskList)
+            .And.ContainEquivalentOf(taskGet1)
+            .And.ContainEquivalentOf(taskGet2);
+    }
+
+    [Fact]
+    public async void TaskService_GetTasksByProjectIdAsync_ThrowsNotFoundExeption_WhenProjectDidntExist()
+    {
+        //Arange
+        _context.Projects.RemoveRange(_context.Projects);
+        await _context.SaveChangesAsync();
+
+        // Act & Assert
+        await Assert.ThrowsAsync<NotFoundException>(
+            () => _taskService.GetTasksByProjectIdAsync(0));
+
+    }
 }
--- Havoc-API/Havoc-API.Tests/TestData/HavocTestContextFactory.cs
+++ Havoc-API/Havoc-API.Tests/TestData/HavocTestContextFactory.cs
@@ -1,7 +1,17 @@
 using System;
 using Havoc_API.Data;
+using Havoc_API.DTOs.Assignment;
+using Havoc_API.DTOs.Attachment;
+using Havoc_API.DTOs.Comment;
+using Havoc_API.DTOs.Tag;
+using Havoc_API.DTOs.Task;
+using Havoc_API.DTOs.TaskStatus;
+using Havoc_API.DTOs.User;
+using Havoc_API.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
+using Task = Havoc_API.Models.Task;
+using TaskStatus = Havoc_API.Models.TaskStatus;
 
 namespace Havoc_API.Tests.TestData;
 
@@ -16,5 +26,99 @@ public static class HavocTestContextFactory
         var databaseContext = new HavocContext(options);
         databaseContext.Database.EnsureCreated();
         return databaseContext;
+    }
+
+    public static User CreateTestUser()
+    {
+        var user = new User("Test", "Test", "test@test.test", "test");
+
+        return user;
+    }
+    public static Project CreateTestProject(User creator)
+    {
+        var project = new Project(
+            "Test",
+            "test",
+            new byte[1234],
+            DateTime.Now,
+            DateTime.Now.AddDays(34),
+            creator,
+            new ProjectStatus("test")
+        );
+
+        return project;
+    }
+
+    public static Task CreateTestTask(User creator, Project project)
+    {
+        var task = new Task(
+            "Test",
+            "Test",
+            DateTime.Now,
+            DateTime.Now.AddDays(34),
+            creator,
+            project,
+            new TaskStatus("test")
+        );
+        
+        return task;
+    }
+
+    public static TaskGET CreateTestTaskGET(Task task)
+    {
+        var taskGet = new TaskGET(
+            task.TaskId,
+            task.Name,
+            task.Description,
+            task.Start,
+            task.Deadline,
+            new UserGET(
+                task.Creator.UserId,
+                task.Creator.FirstName,
+                task.Creator.LastName,
+                task.Creator.Email
+            ),
+            new TaskStatusGET(
+                task.TaskStatus.TaskStatusId,
+                task.TaskStatus.Name
+            ),
+            task.Assignments.Select(assignment => new AssignmentGET(
+                new UserGET(
+                    assignment.UserId,
+                    assignment.User.FirstName,
+                    assignment.User.LastName,
+                    assignment.User.Email
+                ),
+                assignment.Description
+                )).ToList(),
+                task.Attachments.Select(attachment => new AttachmentGET(
+                attachment.AttachmentId,
+                attachment.FileLink,
+                new UserGET(
+                    attachment.UserId,
+                    attachment.User.FirstName,
+                    attachment.User.LastName,
+                    attachment.User.Email
+                )
+            )).ToList(),
+            task.Comments.Select(comment => new CommentGET(
+                comment.CommentId,
+                comment.Content,
+                comment.CommentDate,
+                new UserGET(
+                    comment.UserId,
+                    comment.User.FirstName,
+                    comment.User.LastName,
+                    comment.User.Email
+                )
+            )).ToList(),
+            task.Tags.Select(tag => new TagGET(
+                tag.TagId,
+                tag.Name,
+                tag.ColorHex
+            )).ToList()
+        );
+
+        return taskGet;
     }
 }
